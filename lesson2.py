18778c519116e3b0b7001283f82ae2b646a39840
Avaneeshio <aavaneesh07@gmail.com>
2026-03-14 14:12:57 -0500

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,31 @@
+variable = 23
+variable2 = 23.5
+variable3 = "hello"
+variable4 = False
+variable5 = "Avaneesh"
+variable6 = 14
+variable7 = True
+variable8 = float(variable6)
+variable9 = "43"
+
+print(f"My name is {variable5}")
+print(f"The data type for variable5 is {type(variable5)}")
+print(f"My age is {variable6}")
+print(f"The data type for variable6 is {type(variable6)}")
+print(f"The statement that I am a student is {variable7}")
+print(f"The data type for variable7 is {type(variable7)}")
+print(variable8)
+print(int(variable9))
+#Arithematic operators
+print(variable-variable2)
+print(variable**variable2)
+print(variable+variable2)
+print(variable*variable2)
+print(variable//variable2)
+print(variable/variable2)
+print(variable%variable2)
+#Conditional operators
+print(variable==variable2)
+print(variable!=variable2)
+print(variable>=variable2)
+print(variable<=variable2)
---
+++ lesson2a.py
@@ -0,0 +1,10 @@
+variable1 = "Codingal"
+variable2 = " Education"
+variable3 = " Haa"*5
+variable4 = "Coding"
+print(variable1+variable2)
+print(variable3)
+print(f"The length of variable4 is {len(variable4)}")
+print(f"The first letter of variable4 is {variable4[0]}")
+print(f"The last letter of variable4 is {variable4[5]}")
+print(variable4[0:3])
